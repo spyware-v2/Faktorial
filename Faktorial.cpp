e292eb0a0a9bc66e8f8d48dd6136f9fae53fe1f2
User - D [68] [85] [76] [76] <bdulakun@gmail.com>
2025-01-08 22:19:03 +0700

    Update Faktorial.cpp

--- Faktorial.cpp
+++ Faktorial.cpp
@@ -7,19 +7,24 @@ int main() {
 
     cout << "Masukkan angka : ";
     cin >> a;
-
-
-	if (a<0){
-		cout<<"Faktorial tidak di definisikan untuk bilangan negativ! ";
-	}
 	
     for (int i = 1; i <= a; i++) {
         faktorial *= i; 
     }
-
-    cout << "Faktorial dari " << a << " adalah = " << faktorial << endl;
-    cout << "Alamat memori dari \""<<faktorial<<"\" adalah = "<< &faktorial << endl ;
-    
+	
+	
+	if (a>1){
+		cout << "Faktorial dari " << a << " adalah = " << faktorial << endl;
+    	cout << "Alamat memori dari \""<<faktorial<<"\" adalah = "<< &faktorial << endl ;
+	}
+	
+	else if (a<0){
+		cout<<"Faktorial tidak di definisikan untuk bilangan negativ! ";
+	}
+	
+	else {
+		cout<<"Kesalahan input! hanya dapat menginput angka";
+	}
 
     return 0; 
 }
